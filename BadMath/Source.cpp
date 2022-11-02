89bf9266d679efb44b2407f94f5656ce7302185a
Nickmanzoaerial@gmail.com <Nickmanzoaerial@gmail.com>
2022-11-02 10:35:41 -0400

    Changed the function "average" to work properly by adding parenthesis changed the Cin on Line 22 to take input for n3 instead of n2

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,18 +4,26 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	float avg = (i1 + i2 + i3) / 3;
+	return avg; 
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	int n1;
+	int n2;
+	int n3;
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "First number: ";
+	cin >> n1;
+	cout << "Second number: ";
+	cin >> n2;
+	cout << "Third number: ";
+	cin >> n3; //This was n2 
 
 	float a = average(n1, n2, n3);
+
+	cout << a << endl;
 
 	cout << setprecision(1) << fixed << "The average is " << a << endl;
 
